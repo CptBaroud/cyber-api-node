13e992a39274df3bd44b0ab8868b75a5d4ccbc6a
CptBaroud <gurvan.seveno@gmail.com>
2022-01-05 15:51:43 +0100

    ça peut tout changer

--- app.js
+++ app.js
@@ -78,8 +78,7 @@ const app = express();
 // Extrait de la doc
 // Enable if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB or API Gateway, Nginx, etc)
 // see https://expressjs.com/en/guide/behind-proxies.html
-// app.set('trust proxy', 1);
-
+app.set('trust proxy', 1);
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
     max: 100 // limit each IP to 100 requests per windowMs
